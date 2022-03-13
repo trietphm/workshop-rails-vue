01efe13ed53a2c1558746db88e1b98a4af55ab60
Triet Pham <triet.phm@gmail.com>
2022-03-13 16:12:15 +0700

    init rails + vue

---
+++ config/webpack/production.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+
+const environment = require('./environment')
+
+//module.exports = environment.toWebpackConfig()
+//resolve: {
+//  alias: {
+//    vue: 'vue/dist/vue.js'
+//  }
+//}
